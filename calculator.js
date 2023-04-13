e561dc4f513e20c85453f0a19ef11cf7ebc6ee19
Fineas <r0845664@student.thomasmore.be>
2023-04-13 02:04:31 +0200

    product en deling knop en functie toegevoegd

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@ const getalA=document.getElementById('a');
 const getalB=document.getElementById('b');
 const uitvoer=document.getElementById('uitvoer');
 const optellen=document.getElementById('plus');
-const verschil=document.getElementById('verschil')
+const verschil=document.getElementById('verschil');
+const product=document.getElementById('product');
+const deling=document.getElementById('deling');
 
 function Optellen () {
     let res=parseInt(getalA.value)+parseInt(getalB.value);
@@ -14,5 +16,17 @@ function Verschil () {
     uitvoer.value=res;
 }
 
+function Product () {
+    let res=parseFloat(getalA.value)*parseFloat(getalB.value);
+    uitvoer.value=res;
+}
+
+function Deling () {
+    let res=parseFloat(getalA.value)/parseFloat(getalB.value);
+    uitvoer.value=res;
+}
+
 optellen.addEventListener('click',Optellen);
-verschil.addEventListener('click',Verschil)
+verschil.addEventListener('click',Verschil);
+product.addEventListener('click',Product);
+deling.addEventListener('click',Deling);
